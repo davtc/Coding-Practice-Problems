4e55aad6de8fd33170996b6f0cbae36e4d98020f
davtc <47212365+davtc@users.noreply.github.com>
2022-12-04 16:20:22 +1100

    Added Distinct and MaxProductOfThree lessons

---
+++ Codility - Python/Lesson 6 - MaxProductOfThree.py
@@ -0,0 +1,49 @@
+""" A non-empty array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
+
+For example, array A such that:
+
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+contains the following example triplets:
+
+(0, 1, 2), product is −3 * 1 * 2 = −6
+(1, 2, 4), product is 1 * 2 * 5 = 10
+(2, 4, 5), product is 2 * 5 * 6 = 60
+Your goal is to find the maximal product of any triplet.
+
+Write a function:
+
+def solution(A)
+
+that, given a non-empty array A, returns the value of the maximal product of any triplet.
+
+For example, given array A such that:
+
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+the function should return 60, as the product of triplet (2, 4, 5) is maximal.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [3..100,000];
+each element of array A is an integer within the range [−1,000..1,000].
+Copyright 2009–2022 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited. """
+
+def solution(A):
+    # write your code in Python 3.8.10
+    A_sorted = sorted(A)
+
+    if A_sorted[0] < 0 and A_sorted[1] < 0:
+        return max(A_sorted[0] * A_sorted[1] * A_sorted[-1], A_sorted[-3] * A_sorted[-2] * A_sorted[-1])
+    else:
+        return A_sorted[-3] * A_sorted[-2] * A_sorted[-1]
+
+    pass
